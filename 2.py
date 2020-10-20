d440863d441823d273cd7ed91688b79f5affaf9f
kaweendras <salithak1@gmail.com>
2020-10-20 09:46:00 +0530

    comments

--- 2.py
+++ 2.py
@@ -2,9 +2,11 @@ import cv2
 
 cap = cv2.VideoCapture("resources/video1.mp4")
 
+#loop to show video Frame by Frame
 while True:
     success, img = cap.read()
     cv2.imshow("Video",img)
 
+    # exitst when pressing q
     if cv2.waitKey(1)&0xFF ==ord('q'):
         break
