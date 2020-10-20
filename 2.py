07a75d64199251d0a022f65876a48770482ab5b3
kaweendras <salithak1@gmail.com>
2020-10-20 09:18:08 +0530

    capturing a video

---
+++ 2.py
@@ -0,0 +1,9 @@
+import cv2
+
+cap = cv2.VideoCapture("resources/video1.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1)&0xFF ==ord('q'):
+        break
